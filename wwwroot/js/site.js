e6f994c7e409dd33a8ca9f1ca8c0bcdb2d9d9ac1
Виктория <vita152010@mail.ru>
2022-12-27 15:18:35 +0400

    добавила карусель на главную страницу

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const myCarouselElement = document.querySelector('#mainCarousel');
 
-// Write your Javascript code.
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+});
