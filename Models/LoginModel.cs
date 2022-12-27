7cae79c0f6322924c0e6a39bdcc974d437002316
Виктория <vita152010@mail.ru>
2022-12-27 12:02:20 +0400

    не работает авторизация (проблема с валидацией)

---
+++ Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dog_club.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Не указан логин")]
+        public string UserName { get; set; }
+
+        
+        //[DataType(DataType.Password)]
+        [Required(ErrorMessage = "Не указан пароль")]
+        public string Password { get; set; }
+    }
+}
